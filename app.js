7173c37ab013bca7cb9f533638e884a64251d7a8
Olawole Jethro <kunlekzi2@gmail.com>
2022-12-10 06:11:27 +0100

    rate_limiting

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const passport = require("passport");
 const bodyparser = require("body-parser");
+
+const rateLimiter = require("express-rate-limit");
 const auth_router = require("./Routes/authRouter");
+
 const blogRouter = require("./Routes/blogRouter");
 const userRouter = require("./Routes/userRouter");
 require("dotenv").config;
 
 const app = express();
 app.use(bodyparser.json());
+
+// RATE LIMITTER
+const limiter = rateLimiter({
+  windowMs: 0.5 * 60 * 1000,
+  max: 4,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter);
+
 //INITIALIZING PASSPORT
 app.use(passport.initialize());
 require("./passport");
--- validators/blogValidators.js
+++ validators/blogValidators.js
@@ -1,18 +1,20 @@
 const joi = require("joi");
-joi.ObjectId = require("joi-objectid")(joi);
+// const joiObjectId = require("joi-objectid")(joi);
 
 const blogValidationMiddleware = async (req, res, next) => {
   try {
     await blogSchema.validateAsync(req.body);
     next();
   } catch (error) {
+    console.log("HERE", error);
     return res.status(406).send(error.details[0].message);
+    // return res.status(406).send(error.details[0].message);
   }
 };
 const blogSchema = joi.object({
   title: joi.string().min(3).max(20).required(),
   description: joi.string().min(10).max(200).optional().trim(),
-  // author: joi.objectId().ref("user"),
+  author: joi.ref("user"),
   state: joi.array().has(["published,draft"]).default("draft"),
   read_count: joi.number().default(0),
   reading_time: joi.string(),
